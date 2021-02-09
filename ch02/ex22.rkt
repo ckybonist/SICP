6087f736714b18d0b58fb76c040dd45eff821cec
KuanYu Chu <ckybonist@gmail.com>
2021-02-09 23:14:42 +0800

    Complete ex23; Start writing ch23

---
+++ ch02/ex22.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(provide (all-defined-out))
+
+; util
+(define (avg x y) (exact->inexact (/ (+ x y) 2)))
+
+; Point
+(define (make-point x y) (cons x y))
+(define (x-point point) (car point))
+(define (y-point point) (cdr point))
+(define (is-point? element) (pair? element))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+; Segment
+(define (make-segment . points)
+  (if
+    (ormap (lambda (e) (not (is-point? e))) points) (error 'Error "[Hint] Only _point_ could be the element of segment.")
+    points))
+
+(define (start-segment segment) (car segment))
+(define (end-segment segment) (car (cdr segment)))
+(define (mid-point segment)
+  (let ([s (start-segment segment)] [e (end-segment segment)])
+    (make-point
+      (avg (x-point s) (x-point e))
+      (avg (y-point s) (y-point e)))))
---
+++ ch02/ex23.rkt
@@ -0,0 +1,6 @@
+#lang racket
+
+(require "ex2-2.rkt")
+
+(define p (make-point 3 5))
+(print-point p)
