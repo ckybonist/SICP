1fbb6fcfcdaccf436bdeb578607d5c4c948e78f2
KuanYu Chu <ckybonist@gmail.com>
2021-02-11 15:02:21 +0800

    Add test for exception case of dispatch

--- ch02/ch213.rkt
+++ ch02/ch213.rkt
@@ -8,6 +8,9 @@
           ((= m 1) y)
           (else (error "Argument not 0 or 1: CONS" m))))
   dispatch)
+(check-exn exn:fail?
+  (lambda () ((cons 2 1) 2))
+  "Should raise error if the arugment of dispatch is neither 0 nor 1")
 
 (define (car z) (z 0))
 (define (cdr z) (z 1))
