7a3d9740c0a0311927ca47df5abedb30043563cb
KuanYu Chu <ckybonist@gmail.com>
2021-02-11 13:25:20 +0800

    Implement first rectangle abstraction

--- ch02/ex23.rkt
+++ ch02/ex23.rkt
@@ -14,12 +14,31 @@
     [p1 (make-point (+ width (x-point base-point)) (y-point base-point))]
     [p2 (make-point (x-point base-point) (+ height (y-point base-point)))]
     [p3 (make-point (+ width (x-point base-point)) (+ height (y-point base-point)))])
-    (list base-point p1 p2 p3)))
+    (list base-point p1 p2 p3 (cons width height))))
 
 (check-equal?
   (make-rectangle 10 20 (make-point 0 0))
-  (list (cons 0 0) (cons 10 0) (cons 0 20) (cons 10 20)))
+  (list (cons 0 0) (cons 10 0) (cons 0 20) (cons 10 20) (cons 10 20)))
 
 
-(define (perimeter rectangle) ())
-(define (area rectangle) ())
+(define (width rectangle) (car (last rectangle)))
+(define (height rectangle) (cdr (last rectangle)))
+
+(let ([rectangle (make-rectangle 10 20 (make-point 0 0))])
+  (check-eq? (width rectangle) 10 "width is correct?")
+  (check-eq? (height rectangle) 20 "height is correct?"))
+
+(define (perimeter rectangle)
+  (* 2 (+ (width rectangle) (height rectangle))))
+
+(check-eq?
+  (* 2 (+ 10 20))
+  (perimeter (make-rectangle 10 20 (make-point 0 0))))
+
+
+(define (area rectangle)
+  (* (width rectangle) (height rectangle)))
+
+(check-eq?
+  (* 10 20)
+  (area (make-rectangle 10 20 (make-point 1 1))))
